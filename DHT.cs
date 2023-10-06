d261e6e0e708739c8254617f9ecabcf314c05f8c
HeyManII <chu19972009@gmail.com>
2023-10-06 11:14:32 +0800

    Add files via upload

---
+++ DHT.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DHT : MonoBehaviour
+{
+    InputController sen;
+    public float hum;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        sen = new InputController();
+        sen.Begin("10.66.197.192", 80);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        var hum = sen.CurrentValue;
+        Debug.Log(hum);
+    }
+
+    void OnGUI()
+    {
+        GUI.Label(new Rect(0, 0, 100, 20), "Temperature: "+ hum +"C");
+        GUI.Label(new Rect(0, 15, 100, 20), "Humidity: "+"%");
+    }
+}
---
+++ Inputcontroller.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+using System.Collections.Generic;
+using System.IO;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading;
+ 
+public class InputController
+{
+  public float CurrentValue;
+ 
+  public void Begin(string ipAddress, int port)
+  {
+    // Give the network stuff its own special thread
+    var thread = new Thread(() =>
+    {
+      // This class makes it super easy to do network stuff
+      var client = new TcpClient();
+ 
+      // Change this to your devices real address
+      client.Connect(ipAddress, port);
+      var stream = new StreamReader(client.GetStream());
+ 
+      // We'll read values and buffer them up in here
+      var buffer = new List<byte>();
+      while (client.Connected)
+      {
+        // Read the next byte
+        var read = stream.Read();
+
+          // Once we have a reading, convert our buffer to a string, since the values are coming as strings
+          var str = Encoding.ASCII.GetString(buffer.ToArray());
+
+          // We assume that they're floats
+          var CurrentValue = read.ToString();
+       }
+
+    });  
+    thread.Start();
+  }
+}
